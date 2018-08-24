428a0b3a1f1ed3c49279079e5e72b135765bcb72
xyfwow <40515196+xyfwow@users.noreply.github.com>
2018-08-24 14:46:27 +0800

    Add files via upload

---
+++ shell.sh
@@ -0,0 +1,5 @@
+mpirun -np 12 ../src/pfc step1
+mpirun -np 12 ../src/pfc step2
+java -jar coordinator.jar step2-t\:10000.dat 1024 1024 0.7 0.7 7.3 2.46 step2-t\:10000.xy step2-t\:10000.nh
+java -jar plotter.jar step2-t:# step2-t:# 1024 1024 0 1000 10000
+awk '{print $1,1,$2,$3,0}' step2-t\:10000.xy > graphene.xyz
