5368dddd42a3db47ef05791e4390c8a6b50ffe7e
xyfwow <40515196+xyfwow@users.noreply.github.com>
2018-08-25 23:04:28 +0800

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 mpirun -np 12 ../src/pfc step1
 mpirun -np 12 ../src/pfc step2
 java -jar coordinator.jar step2-t\:10000.dat 1024 1024 0.7 0.7 7.3 2.46 step2-t\:10000.xy step2-t\:10000.nh
